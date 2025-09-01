19a0c3f8abc992f05cd773337f3a9fc73c48e60e
mayaraa2007 <00001107865967sp@al.educacao.sp.gov.br>
2025-09-01 17:17:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,40 @@ document.addEventListener('DOMContentLoaded', function () {
     botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+        // acessibilidade aria-expanded
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
     });
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById('alterna-contraste');
 
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
-        if (tamanhoAtualFonte < 2) { // limite máximo (exemplo)
+        if (tamanhoAtualFonte < 2) { // limite máximo
             tamanhoAtualFonte += 0.1;
             document.body.style.fontSize = `${tamanhoAtualFonte.toFixed(1)}rem`;
         }
     });
 
     diminuiFonteBotao.addEventListener('click', function () {
-        if (tamanhoAtualFonte > 0.5) { // limite mínimo (exemplo)
+        if (tamanhoAtualFonte > 0.5) { // limite mínimo
             tamanhoAtualFonte -= 0.1;
             document.body.style.fontSize = `${tamanhoAtualFonte.toFixed(1)}rem`;
         }
     });
+
+    // alternar contraste
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste');
+    });
 });
+
+// Animações ScrollReveal
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#historia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
